8d2b6dce899c69d0c07fa686ceb51d19367ddcf7
d-stoll <danielsimon.stoll2@gmail.com>
2019-05-19 00:54:20 +0200

    Monitor completed

--- Monitor.py
+++ Monitor.py
@@ -1,9 +1,12 @@
 from multiprocessing import Process, Queue
 from TimeControl import TimeControl
+import threading
 import os
 from Sensors.Photoresistor import Photoresistor
 from MediaDevices.MusicPlayer import MusicPlayer
 from MediaDevices.Lightstrip import Lightstrip
+from Models.PhotoresistorDto import PSignal
+from Models.MusicDto import MusicCmd
 
 # TODO: Build Processes with Queues for IPC, start them and manage the Communication.
 # TODO: Repair after Subprocess failes.
@@ -39,10 +42,62 @@ class Monitor(Process):
         self.queue_ls = Queue()
         self.lightstrip = Lightstrip(LED_COUNT, LED_PIN, self.queue_ls)
 
+        self.timer = None
+
+    # Reagiert auf Events von der Lichtschranke
+    def react_on_event(self):
+        if self.state == 0:
+            self.state += 1
+            self.queue_mp.put(MusicCmd.PLAY)
+            self.queue_ls.put(self.state)
+            self.start_timer()
+        elif self.state < 20:
+            self.state += 1
+            self.queue_ls.put(self.state)
+        else:
+            print(f"Counter: {self.state} -> Maximum erreicht")
+
+        # Target-Funktion fuer den Timer
+    def target_function(self):
+        print("---------------------")
+        print("Timer abgelaufen")
+        if self.state == 1:
+            self.state -= 1
+            self.queue_mp.put(MusicCmd.STOP)
+            self.queue_ls.put(self.state)
+        elif self.state >= 1:
+            self.state -= 1
+            self.start_timer()
+            self.queue_ls.put(self.state)
+        print("Counter: ", self.state)
+        print("---------------------")
+
+    # Startet den Timer
+    def start_timer(self):
+        # TIMER STOPPEN
+        if self.timer is not None:
+            self.timer.cancel()
+            self.timer = None
+        self.timer = threading.Timer(TIMER_DURATION, self.target_function)
+        self.timer.start()
+
     def run(self) -> None:
         self.photo_resistor.start()
         self.music_player.start()
         self.lightstrip.start()
+
+        while True:
+            motion = self.queue_pr.get()
+            if not isinstance(motion, PSignal):
+                self.restart_photoresistor()
+            self.react_on_event()
+
+    def restart_photoresistor(self):
+        del self.queue_pr
+        del self.photo_resistor
+        self.queue_pr = Queue()
+        self.photo_resistor = Photoresistor(PHOTO_PIN, PHOTO_BOUNCETIME, self.queue_pr)
+        self.photo_resistor.start()
 
 
 if __name__ == '__main__':
